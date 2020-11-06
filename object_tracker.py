5e5758def749bd80a82a0bec9074c3ebd15612d3
“your <malithi.mith@gmail.com>
2020-11-06 12:41:54 +0530

    first commit

---
+++ object_tracker.py
@@ -0,0 +1,121 @@
+# USAGE
+# python object_tracker.py --prototxt deploy.prototxt --model res10_300x300_ssd_iter_140000.caffemodel
+
+# import the necessary packages
+from pyimagesearch.centroidtracker import CentroidTracker
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+ap.add_argument("-i", "--input", type=str,
+	help="path to optional input video file")
+ap.add_argument("-o", "--output", type=str,
+	help="path to optional output video file")
+ap.add_argument("-s", "--skip-frames", type=int, default=30,
+	help="# of skip frames between detections")
+args = vars(ap.parse_args())
+
+
+# initialize our centroid tracker and frame dimensions
+ct = CentroidTracker()
+(H, W) = (None, None)
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+# initialize the video stream and allow the camera sensor to warmup
+if not args.get("input", False):
+	print("[INFO] starting video stream...")
+	vs = VideoStream(src=0).start()
+	time.sleep(2.0)
+# otherwise, grab a reference to the video file
+else:
+	print("[INFO] opening video file...")
+	vs = cv2.VideoCapture(args["input"])
+
+# loop over the frames from the video stream
+while True:
+	# grab the next frame and handle if we are reading from either
+	# VideoCapture or VideoStream
+	frame = vs.read()
+	frame = frame[1] if args.get("input", False) else frame
+	# if we are viewing a video and we did not grab a frame then we
+	# have reached the end of the video
+	if args["input"] is not None and frame is None:
+		break
+	# resize the frame to have a maximum width of 500 pixels (the
+	# less data we have, the faster we can process it), then convert
+	# the frame from BGR to RGB for dlib
+	frame = imutils.resize(frame, width=500)
+	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	# if the frame dimensions are empty, set them
+	if W is None or H is None:
+		(H, W) = frame.shape[:2]
+	# if we are supposed to be writing a video to disk, initialize
+	# the writer
+	if args["output"] is not None and writer is None:
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		writer = cv2.VideoWriter(args["output"], fourcc, 30,
+			(W, H), True)
+
+	# construct a blob from the frame, pass it through the network,
+	# obtain our output predictions, and initialize the list of
+	# bounding box rectangles
+	blob = cv2.dnn.blobFromImage(frame, 1.0, (W, H),
+		(104.0, 177.0, 123.0))
+	net.setInput(blob)
+	detections = net.forward()
+	rects = []
+
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		# filter out weak detections by ensuring the predicted
+		# probability is greater than a minimum threshold
+		if detections[0, 0, i, 2] > args["confidence"]:
+			# compute the (x, y)-coordinates of the bounding box for
+			# the object, then update the bounding box rectangles list
+			box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+			rects.append(box.astype("int"))
+
+			# draw a bounding box surrounding the object so we can
+			# visualize it
+			(startX, startY, endX, endY) = box.astype("int")
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				(0, 255, 0), 2)
+
+	# update our centroid tracker using the computed set of bounding
+	# box rectangles
+	objects = ct.update(rects)
+
+	# loop over the tracked objects
+	for (objectID, centroid) in objects.items():
+		# draw both the ID of the object and the centroid of the
+		# object on the output frame
+		text = "ID {}".format(objectID)
+		cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+		cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
